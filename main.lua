0981320b71c410cb82e61e91000b5ed66c249f5a
wtrstylus-331 <saisrikara5@gmail.com>
2025-01-21 18:47:13 -0500

    Implemented logic into update loop

--- main.lua
+++ main.lua
@@ -1,17 +1,33 @@
-player = require("player")
-running = false
+playerClass = require("player")
+pipeClass = require("pipe")
 
 function love.load()
-    plr = player.new()
+    plr = playerClass.new()
     plr:Center()
+
+    score = 0
+    highscore = 0
+    running = true
+    action = false
 end
 
 function love.update(dt)
     if running == true then
-        plr.y = plr.y + 0.5
+        plr:Update(dt)
+
+        if action then
+            plr:SetYVelocity(-150)  -- Apply upward velocity when the mouse is clicked
+            action = false   -- Reset the click state to avoid continuous jumping
+        end
     end
 end
 
 function love.draw()
     love.graphics.draw(plr:GetSprite(), plr.x, plr.y, 0, 0.075, 0.075)
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    if button == 1 then  -- Left mouse button (button 1 is usually the left button)
+        action = true
+    end
 end
