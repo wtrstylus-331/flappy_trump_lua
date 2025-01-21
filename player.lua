a9aefc6d9ff6d0baf1709d47a7e9773db2f6b522
wtrstylus-331 <saisrikara5@gmail.com>
2025-01-21 18:03:57 -0500

    Variables and method for player

--- player.lua
+++ player.lua
@@ -1,3 +1,5 @@
+require "love"
+
 Player = {}
 PlayerMeta = {__index = Player}
 
@@ -5,14 +7,23 @@ function Player.new()
     local instance = {}
     setmetatable(instance, PlayerMeta)
 
-    instance.name = "test"
+    instance.x = 0
+    instance.y = 0
+    instance.width = 64
+    instance.height = 64
+    instance.sprite = love.graphics.newImage("assets/player.png")
+    --instance.sprite:setFilter("nearest", "nearest")
 
     return instance
 end
 
--- debug
-function Player:Print()
-    print(self.name)
+function Player:GetSprite()
+    return self.sprite
+end
+
+function Player:Center()
+    self.x = (love.graphics.getPixelWidth() / 2) - (self.width / 2)
+    self.y = (love.graphics.getPixelHeight() / 2) - (self.height / 2)
 end
 
 return Player
