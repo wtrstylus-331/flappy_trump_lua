800e669d6503d0323c8b40d64bfe10065860d655
wtrstylus-331 <saisrikara5@gmail.com>
2025-01-21 18:46:41 -0500

    YVelocity and falling logic implemented

--- player.lua
+++ player.lua
@@ -9,6 +9,7 @@ function Player.new()
 
     instance.x = 0
     instance.y = 0
+    instance.yVelocity = 0
     instance.width = 64
     instance.height = 64
     instance.sprite = love.graphics.newImage("assets/player.png")
@@ -24,6 +25,20 @@ end
 function Player:Center()
     self.x = (love.graphics.getPixelWidth() / 2) - (self.width / 2)
     self.y = (love.graphics.getPixelHeight() / 2) - (self.height / 2)
+end
+
+function Player:Update(dt)
+    self.yVelocity = self.yVelocity + 40 * 5 * dt
+
+    if self.yVelocity > 500 then
+        self.yVelocity = 500
+    end
+
+    self.y = self.y + self.yVelocity * dt
+end
+
+function Player:SetYVelocity(velocity)
+    self.yVelocity = velocity
 end
 
 return Player
