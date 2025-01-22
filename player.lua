cc607a92140c75304e6ef2360727f57d05d54d5e
wtrstylus-331 <saisrikara5@gmail.com>
2025-01-21 21:32:44 -0500

    Rotation logic for player

--- player.lua
+++ player.lua
@@ -10,10 +10,10 @@ function Player.new()
     instance.x = 0
     instance.y = 0
     instance.yVelocity = 0
+    instance.rotation = 0
     instance.width = 64
     instance.height = 64
     instance.sprite = love.graphics.newImage("assets/player.png")
-    --instance.sprite:setFilter("nearest", "nearest")
 
     return instance
 end
@@ -34,11 +34,18 @@ function Player:Update(dt)
         self.yVelocity = 500
     end
 
+    if self.rotation < 0.8 then
+        self.rotation = self.rotation + 0.4 * 1.05 * dt
+        self.x = self.x + 20 * dt
+    end
+
     self.y = self.y + self.yVelocity * dt
 end
 
 function Player:SetYVelocity(velocity)
     self.yVelocity = velocity
+    self.rotation = -0.25
+    self.x = (love.graphics.getPixelWidth() / 2) - (self.width / 2)
 end
 
 return Player
