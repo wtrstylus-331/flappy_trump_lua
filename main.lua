12ce4f0f677346ade3935d952924f55c649c8b0e
wtrstylus-331 <saisrikara5@gmail.com>
2025-01-25 19:20:31 -0500

    Implement score handling and quit UI

--- main.lua
+++ main.lua
@@ -14,9 +14,14 @@ function love.load()
 
     play = {}
     replay = {}
+    quit = {}
     playTxt = {}
     replayTxt = {}
+    quitTxt = {}
     gamePipes = {}
+    scoreTxt = {}
+    highscoreTxt = {}
+    scoreCounter = {}
 
     plr = playerClass.new()
     plr:Center()
@@ -48,13 +53,24 @@ function love.load()
     replay.width = 200
     replay.height = 70
     replay.x = (love.graphics.getPixelWidth() / 2) - (175 / 2)
-    replay.y = 470
+    replay.y = 450
 
     replayTxt.text = love.graphics.newText(love.graphics.newFont(40), "RESTART")
     replayTxt.x = replay.x + 12
     replayTxt.y = replay.y + 10
 
-    score = 0
+    quit.width = 200
+    quit.height = 45
+    quit.x = (love.graphics.getPixelWidth() / 2) - (175 / 2)
+    quit.y = 535
+
+    quitTxt.text = love.graphics.newText(love.graphics.newFont(30), "QUIT")
+    quitTxt.x = quit.x + 57
+    quitTxt.y = quit.y + 5.25
+
+    scoreCounter.font = love.graphics.newFont("assets/pricedown_bl.otf", 90)
+    scoreCounter.score = 0
+
     highscore = 0
     counter = 0
     running = false
@@ -71,7 +87,7 @@ function love.update(dt)
             
             if counter >= 810 then
                 p = pipeClass.new()
-                p:SetYPos(math.random(-250, 0))
+                p:SetYPos(math.random(-180, -10))
                 table.insert(gamePipes, p)
                 counter = 0
             end
@@ -81,6 +97,10 @@ function love.update(dt)
 
                 if p:DetectCollision(plr) == true then
                     die()
+                end
+
+                if p:Passed(plr) == true then
+                    scoreCounter.score = scoreCounter.score + 1
                 end
             end
 
@@ -106,74 +126,140 @@ end
 
 function love.draw()
     if title == true then
-        -- background
+        drawTitle()
+    end
+
+    if running == true then
+        drawGameEssentials()
+        drawScore()
+    end
+
+    if paused == true then
+        drawPausedEssentials()
+    end
+end
+
+function drawTitle()
+    -- background
+    love.graphics.setColor(1,1,1)
+    love.graphics.draw(titleBg, 0, 0, 0, 0.7, 0.9)
+    
+    -- icon
+    love.graphics.setColor(1,1,1)
+    love.graphics.draw(icon, 520, 15, 0.2, 0.2, 0.2)
+
+    -- title text
+    love.graphics.setColor(1,1,1)
+    love.graphics.draw(titleTxt, 125, 50, 0, 0.9, 0.9)
+
+    -- btn border
+    love.graphics.setColor(0,0,0)
+    love.graphics.setLineWidth(10)
+    love.graphics.rectangle("line", play.x, play.y, play.width, play.height)
+
+    -- btn highlight + fill
+    if love.mouse.getX() >= play.x and love.mouse.getX() <= play.x + play.width and love.mouse.getY() >= play.y and love.mouse.getY() <= play.y + play.height then
+        love.graphics.setColor(110/255, 110/255, 110/255)
+    else
+        love.graphics.setColor(220/255, 220/255, 220/255)
+    end
+    love.graphics.rectangle("fill", play.x, play.y, play.width, play.height)  
+
+    -- btn text
+    love.graphics.setColor(0,0,0)
+    love.graphics.draw(playTxt.text, playTxt.x, playTxt.y)
+end
+
+function drawScore()
+    -- stroke color
+    love.graphics.setColor(0, 0, 0)
+
+    local x
+    local y = 15
+
+    if string.len(tostring(scoreCounter.score)) > 2 then
+        x = 363 
+    else
+        x = 378
+    end
+
+    local text = tostring(scoreCounter.score)
+
+    -- draw stroke
+    love.graphics.setFont(scoreCounter.font)
+    love.graphics.print(text, x - 4, y)
+    love.graphics.print(text, x + 4, y)
+    love.graphics.print(text, x, y - 4)
+    love.graphics.print(text, x, y + 4)
+
+    love.graphics.print(text, x - 4, y - 4)
+    love.graphics.print(text, x + 4, y - 4)
+    love.graphics.print(text, x - 4, y + 4)
+    love.graphics.print(text, x + 4, y + 4)
+
+    -- fill text color
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print(text, x, y)
+end
+
+
+function drawGameEssentials()
+    -- bg
+    love.graphics.setColor(1,1,1)
+    love.graphics.draw(gameBg, 0, 0, 0, 0.5, 0.6)
+
+    -- player
+    love.graphics.setColor(1,1,1)
+    love.graphics.draw(plr:GetSprite(), plr.x, plr.y, plr.rotation, 0.075, 0.075)
+
+    -- pipes
+    for i, pipe in ipairs(gamePipes) do
         love.graphics.setColor(1,1,1)
-        love.graphics.draw(titleBg, 0, 0, 0, 0.7, 0.9)
+        love.graphics.draw(pipe:GetSprite("top"), pipe.topX, pipe.topY, 0, 0.4, 0.4)
+        love.graphics.draw(pipe:GetSprite("bottom"), pipe.bottomX, pipe.bottomY, 0, 0.4, 0.4)
+    end
+end
+
+function drawPausedEssentials()
+    if dead then
+        -- label
+        love.graphics.setColor(1,1,1)
+        love.graphics.draw(deadLabel, 235, 330, 0, 0.9, 0.9)
         
-        -- icon
-        love.graphics.setColor(1,1,1)
-        love.graphics.draw(icon, 520, 15, 0.2, 0.2, 0.2)
-
-        -- title text
-        love.graphics.setColor(1,1,1)
-        love.graphics.draw(titleTxt, 125, 50, 0, 0.9, 0.9)
-
         -- btn border
         love.graphics.setColor(0,0,0)
         love.graphics.setLineWidth(10)
-        love.graphics.rectangle("line", play.x, play.y, play.width, play.height)
+        love.graphics.rectangle("line", replay.x, replay.y, replay.width, replay.height)
+
+        love.graphics.setColor(0,0,0)
+        love.graphics.setLineWidth(10)
+        love.graphics.rectangle("line", quit.x, quit.y, quit.width, quit.height)
 
         -- btn highlight + fill
-        if love.mouse.getX() >= play.x and love.mouse.getX() <= play.x + play.width and love.mouse.getY() >= play.y and love.mouse.getY() <= play.y + play.height then
+        if love.mouse.getX() >= replay.x and love.mouse.getX() <= replay.x + replay.width and love.mouse.getY() >= replay.y and love.mouse.getY() <= replay.y + replay.height then
             love.graphics.setColor(110/255, 110/255, 110/255)
         else
             love.graphics.setColor(220/255, 220/255, 220/255)
         end
-        love.graphics.rectangle("fill", play.x, play.y, play.width, play.height)  
+        love.graphics.rectangle("fill", replay.x, replay.y, replay.width, replay.height)  
+
+        if love.mouse.getX() >= quit.x and love.mouse.getX() <= quit.x + quit.width and love.mouse.getY() >= quit.y and love.mouse.getY() <= quit.y + quit.height then
+            love.graphics.setColor(110/255, 110/255, 110/255)
+        else
+            love.graphics.setColor(220/255, 220/255, 220/255)
+        end
+        love.graphics.rectangle("fill", quit.x, quit.y, quit.width, quit.height)  
 
         -- btn text
         love.graphics.setColor(0,0,0)
-        love.graphics.draw(playTxt.text, playTxt.x, playTxt.y)
-    end
+        love.graphics.draw(replayTxt.text, replayTxt.x, replayTxt.y)
 
-    if running == true then
+        love.graphics.setColor(0,0,0)
+        love.graphics.draw(quitTxt.text, quitTxt.x, quitTxt.y)
+    else 
+        -- instruction label
         love.graphics.setColor(1,1,1)
-        love.graphics.draw(gameBg, 0, 0, 0, 0.7, 0.7)
-
-        love.graphics.draw(plr:GetSprite(), plr.x, plr.y, plr.rotation, 0.075, 0.075)
-
-        for i, pipe in ipairs(gamePipes) do
-            love.graphics.setColor(1,1,1)
-            love.graphics.draw(pipe:GetSprite("top"), pipe.topX, pipe.topY, 0, 0.4, 0.4)
-            love.graphics.draw(pipe:GetSprite("bottom"), pipe.bottomX, pipe.bottomY, 0, 0.4, 0.4)
-        end
-    end
-
-    if paused == true then
-        if dead then
-            love.graphics.setColor(1,1,1)
-            love.graphics.draw(deadLabel, 235, 330, 0, 0.9, 0.9)
-            
-            -- btn border
-            love.graphics.setColor(0,0,0)
-            love.graphics.setLineWidth(10)
-            love.graphics.rectangle("line", replay.x, replay.y, replay.width, replay.height)
-    
-            -- btn highlight + fill
-            if love.mouse.getX() >= replay.x and love.mouse.getX() <= replay.x + replay.width and love.mouse.getY() >= replay.y and love.mouse.getY() <= replay.y + replay.height then
-                love.graphics.setColor(110/255, 110/255, 110/255)
-            else
-                love.graphics.setColor(220/255, 220/255, 220/255)
-            end
-            love.graphics.rectangle("fill", replay.x, replay.y, replay.width, replay.height)  
-    
-            -- btn text
-            love.graphics.setColor(0,0,0)
-            love.graphics.draw(replayTxt.text, replayTxt.x, replayTxt.y)
-        else 
-            love.graphics.setColor(1,1,1)
-            love.graphics.draw(label, 210, 330, 0, 0.9, 0.9)
-        end
+        love.graphics.draw(label, 210, 330, 0, 0.9, 0.9)
     end
 end
 
@@ -192,7 +278,12 @@ function love.mousepressed(x, y, button, istouch, presses)
                         paused = true
                         dead = false
                         clearPipes()
+                        scoreCounter.score = 0
                         plr:Center()
+                    end
+
+                    if x >= quit.x and x <= quit.x + quit.width and y >= quit.y and y <= quit.y + quit.height then
+                        love.event.quit()
                     end
                 end
             end
